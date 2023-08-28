00febd354ef279d29889a737ec5b0da314fbbb08
singhrishabh93 <singhrishabh1670@gmal.com>
2023-08-28 21:04:19 +0530

    Added Project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'carbon_footprint_calculator.dart';
+import 'circular_button.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Carbon Footprint Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CarbonFootprintCalculator(),
+    );
+  }
+}
